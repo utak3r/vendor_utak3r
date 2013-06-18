d0b68fefad58f9fcef3f83cf238712909e4fe960
utak3r <piotr.borys@orad.pl>
2013-06-18 11:31:01 +0200

    Initial commit

---
+++ gapps.mk
@@ -0,0 +1,19 @@
+
+PRODUCT_COPY_FILES += \
+vendor/utak3r/prebuilt/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar \
+vendor/utak3r/prebuilt/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
+vendor/utak3r/prebuilt/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar \
+vendor/utak3r/prebuilt/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+vendor/utak3r/prebuilt/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
+vendor/utak3r/prebuilt/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
+vendor/utak3r/prebuilt/etc/permissions/features.xml:system/etc/permissions/features.xml \
+vendor/utak3r/prebuilt/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
+vendor/utak3r/prebuilt/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
+vendor/utak3r/prebuilt/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+vendor/utak3r/prebuilt/app/GoogleFeedback.apk:system/app/GoogleFeedback.apk \
+vendor/utak3r/prebuilt/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
+vendor/utak3r/prebuilt/app/GooglePartnerSetup.apk:system/app/GooglePartnerSetup.apk \
+vendor/utak3r/prebuilt/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
+vendor/utak3r/prebuilt/app/MediaUploader.apk:system/app/MediaUploader.apk \
+vendor/utak3r/prebuilt/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
+vendor/utak3r/prebuilt/app/SetupWizard.apk:system/app/SetupWizard.apk
---
+++ u3addons.mk
@@ -0,0 +1,4 @@
+-include vendor/utak3r/gapps.mk
+
+PRODUCT_COPY_FILES += \
+vendor/utak3r/prebuilt/app/FileManager.apk:system/app/FileManager.apk
